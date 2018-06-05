390c291c12a68c4790abd40f02b50962318bf6a8
Jose Anasco <joseanasco1@gmail.com>
2018-06-05 15:10:47 -0400

    remove not included files/folder from mix file

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Ulid.Mixfile do
 
   defp package do
     [name: :ulid,
-     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     files: ["lib", "mix.exs", "README*"],
      maintainers: ["Jose Añasco"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/merongivian/ulid"}]
