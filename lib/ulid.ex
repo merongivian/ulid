087cd8cf78027914e2b2ff88b303563ae542a7f7
Matthew Johnston <warmwaffles@gmail.com>
2019-08-07 13:55:55 -0500

    Add some type specs to methods

--- lib/ulid.ex
+++ lib/ulid.ex
@@ -1,9 +1,13 @@
 defmodule Ulid do
-  def generate(timestamp \\ System.system_time(:milli_seconds)) do
+  @type t :: binary()
+
+  @spec generate(pos_integer()) :: t()
+  def generate(timestamp \\ System.system_time(:millisecond)) do
     Ulid.Utils.encode(generate_binary(timestamp))
   end
 
-  def generate_binary(timestamp \\ System.system_time(:milli_seconds)) do
+  @spec generate_binary(pos_integer()) :: t()
+  def generate_binary(timestamp \\ System.system_time(:millisecond)) do
     <<timestamp::unsigned-size(48), :crypto.strong_rand_bytes(10)::binary>>
   end
 end
